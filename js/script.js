1c61abfa27d37a76e979def693c45863a63dc0f7
appitsolution <appitsolution@gmail.com>
2023-12-04 19:23:27 +0200

    test

--- js/script.js
+++ js/script.js
@@ -21,55 +21,23 @@ leaveRequestButton2.addEventListener("click", changeModalWindow);
 closeButton.addEventListener("click", changeModalWindow);
 
 const submitForm = () => {
-  function start() {
-    gapi.client
-      .init({
-        apiKey: "aGOCSPX-4i1SvB8hprKAv1LeRrua_cZZ0-yM",
-        clientId:
-          "663713516697-qbest32iifbljj34dqihc14b2if7s40f.apps.googleusercontent.com",
-        scope: "profile",
-      })
-      .then(function () {
-        return gapi.client.people.people.get({
-          resourceName: "people/me",
-          "requestMask.includeField": "person.names",
-        });
-      })
-      .then(
-        function (response) {
-          console.log(response.result);
+  const spreadsheetId = "test-table-407114";
+  const range = "List1!A:A";
+  const apiKey = "aGOCSPX-4i1SvB8hprKAv1LeRrua_cZZ0-yM";
 
-          const accessToken = gapi.auth2
-            .getAuthInstance()
-            .currentUser.get()
-            .getAuthResponse().access_token;
-          const spreadsheetId = "test-table-407114";
-          const range = "List1!A:A";
-
-          fetch(
-            `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}:append?valueInputOption=USER_ENTERED`,
-            {
-              method: "POST",
-              headers: {
-                Authorization: `Bearer ${accessToken}`,
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                values: [response.result.names[0].displayName],
-              }),
-            }
-          )
-            .then((response) => response.json())
-            .then((data) => console.log(data))
-            .catch((error) => console.error("Error:", error));
-        },
-        function (reason) {
-          console.dir(reason);
-        }
-      );
-  }
-
-  gapi.load("client", start);
+  fetch(
+    `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}:append?valueInputOption=USER_ENTERED&key=${apiKey}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ values: [["data1", "data2"]] }),
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Error:", error));
 };
 
 sendButton.addEventListener("click", submitForm);
